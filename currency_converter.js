1b6931ff3562a4a0d5a7657e833e89407d6330a2
= <officiallyaninja@gmail.com>
2023-10-01 22:09:55 +0530

    added wage and time calculator stub

--- currency_converter.js
+++ currency_converter.js
@@ -2,7 +2,7 @@
  * converts currencies into the users currency
  * @param {string} text_currency - the currency of the text element (eg: "USD", "INR" etc)
  * @param {string} user_currency - the currency of the user (eg: "USD", "INR" etc)
- * @param {number} amount - the amount of the texr currency
+ * @param {number} amount - the amount of the text currency
  * @returns {number} the amount in the users currency
  */
 function convert(text_currency, user_currency, amount) {
---
+++ hourly_wage_calculator.js
@@ -0,0 +1,27 @@
+/**
+ * converts an annual salary into an hourly wage
+ * @param {number} hours_worked_per_week - hours the user works in a week
+ * @param {number} annual_salary - users annual salary
+ * @returns {number} the hourly wage the user earns
+ */
+function hourly_from_annual(hours_worked_per_week, annual_salary) {
+}
+
+/**
+ * converts an monthly salary into an hourly wage
+ * @param {number} hours_worked_per_week - hours the user works in a week
+ * @param {number} monthly_salary - users annual salary
+ * @returns {number} the hourly wage the user earns
+ */
+function hourly_from_monthly(hours_worked_per_week, monthly_salary) {
+}
+
+/**
+ * converts an monthly salary into an hourly wage
+ * @param {number} monetary_cost - the cost of the item
+ * @param {number} monthly_salary - the users hourly wage
+ * @returns {number} the amount of minutes the user needs to work to earn this much money (integer)
+ */
+function money_to_time(monetary_cost, hourly_wage) {
+
+}
