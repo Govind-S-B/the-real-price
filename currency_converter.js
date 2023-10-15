27a13f8bcd95c2039ad0e911504b88a0f027a8f1
Vasanthi B N S <127195382+VB-123@users.noreply.github.com>
2023-10-15 21:05:38 +0530

    Fixed currency_converter.js

--- currency_converter.js
+++ currency_converter.js
@@ -1,51 +1,58 @@
 import axios from 'axios';
 import storage from 'node-persist';
-import dotenv from 'dotenv';
-dotenv.config({ path: './api.env' });
+
 
 // Initialize storage
 await storage.init({ dir: './Extension/data', ttl: 86400000 });
 
 /**
  * Converts currencies into the user's currency
+ * @param {string} cacheKey (eg: "USD_JPY" etc)
+ * @returns {bool} - It tells the convert function whether to update the cache or not
+ */
+
+async function shouldUpdateCache(cacheKey) {
+  const cachedData = await storage.getItem(cacheKey);
+  if (cachedData && (Date.now() - cachedData.timestamp < 86400000)) {
+    return false; // Cache is still valid
+  }
+  return true; // Cache needs to be updated
+}
+
+/**
+ * Converts currencies into the user's currency
  * @param {string} text_currency - the currency of the text element (eg: "USD", "INR" etc)
  * @param {string} user_currency - the currency of the user (eg: "USD", "INR" etc)
  * @param {number} amount - the amount of the text currency
- * @returns {Promise<number>} - a promise that resolves to the amount in the user's currency
+ * @returns {number} - the converted amount is returned
  */
 
 async function convert(text_currency, user_currency, amount) {
-  const apiKey = process.env.API_KEY;
-
   // Check if rates are available in the storage
   const cacheKey = `${text_currency}_${user_currency}`;
-  const cachedRates = await storage.getItem(cacheKey);
+  const cachedData = await storage.getItem(cacheKey);
 
-  if (cachedRates) {
-    const convertedAmount = amount * cachedRates[user_currency];
+  if (!(await shouldUpdateCache(cacheKey))) {
+    const convertedAmount = (amount * cachedData.rates[user_currency])/cachedData.rates[text_currency];
     console.log("Present in cache\n"); //debug
     return convertedAmount;
   }
 
   try {
-    // Make API request to get the conversion rate
-    const response = await axios.get(`https://api.currencyfreaks.com/v2.0/rates/latest`, {
-      params: {
-        apikey: apiKey,
-      },
-    });
+    // Make API request to get the conversion rate for the specific input currencies
+    const response = await axios.get(`https://api.frankfurter.app/latest?to=${user_currency},${text_currency}`);
     
     if (response.status === 200) {
       const { rates } = response.data;
 
       // Update rates in the storage
-      const updatedRates = {
-        ...rates,
-        [text_currency]: 1, // Add the base currency rate to the rates object
+      const newData = {
+        timestamp: Date.now(),
+        rates: rates
       };
-      await storage.setItem(cacheKey, updatedRates);
+      await storage.setItem(cacheKey, newData);
       console.log(await storage.getItem(cacheKey)); //debug
-      const convertedAmount = amount * updatedRates[user_currency];
+      const convertedAmount = amount * rates[user_currency];
       return convertedAmount;
     } 
     else {
@@ -55,12 +62,21 @@ async function convert(text_currency, user_currency, amount) {
   
   catch (error) {
     console.error(error);
-    throw new Error('Failed to convert currencies');
+    if (cachedData) {
+      // If API call fails, use the old cache data
+      const convertedAmount = amount * cachedData.rates[user_currency];
+      return convertedAmount;
+    } 
+    else {
+      throw new Error('Failed to convert currencies');
+    }
   }
 }
+  
+  
 
-// The test
-const text_currency = 'GBP';
+// A test case
+const text_currency = 'USD';
 const user_currency = 'JPY';
 const amount = 25;
 
