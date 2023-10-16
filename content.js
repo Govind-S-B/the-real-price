46923a1539c2f55898f913b8d4cb9b587623d9ea
Arjun Jayakumar <arjun.jayan@gmail.com>
2023-10-16 17:20:22 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,11 +1,22 @@
-function replaceText() {
-    let bodyText = document.body.innerHTML;
-    bodyText = bodyText.replace(/x/gi, '6969');
-    document.body.innerHTML = bodyText;
-}
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "replace") {
-        replaceText();
+function replaceCurrency(text, placeholder = '***') {
+    let regex = /(\$|€|£|RS|USD|EUR|)\s*\d{1,4}(?:,\d{3})*(?:\.\d{2})?/g;
+    return text.replace(regex, placeholder);
+  }
+  function replaceText() {
+    let walker = document.createTreeWalker(
+      document.body,
+      NodeFilter.SHOW_TEXT,
+      null,
+      false
+    );
+    let node;
+    while((node = walker.nextNode())) {
+      
+      node.textContent = replaceCurrency(node.textContent);
     }
-});
+  }
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "replace") {
+      replaceText();
+    }
+  });
